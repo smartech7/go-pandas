pandas-dev/pandas

Add SparseSeries.to_coo and from_coo methods for interaction with scipy.sparse.

vb_suite/sparse.py
@@ -37,3 +37,29 @@ stmt = "SparseDataFrame(columns=np.arange(100), index=np.arange(1000))"
 
 sparse_constructor = Benchmark(stmt, setup, name="sparse_frame_constructor",
                                start_date=datetime(2012, 6, 1))
+
+
+setup = common_setup + """
+s = pd.Series([nan] * 10000)
+s[0] = 3.0
+s[100] = -1.0
+s[999] = 12.1
+s.index = pd.MultiIndex.from_product((range(10), range(10), range(10), range(10)))
+ss = s.to_sparse()
+"""
+
+stmt = "ss.to_coo(ilevels=[0, 1], jlevels=[2, 3], sort_labels=True)"
+
+sparse_series_to_coo = Benchmark(stmt, setup, name="sparse_series_to_coo",
+                               start_date=datetime(2015, 1, 3))
+
+setup = common_setup + """
+import scipy.sparse
+import pandas.sparse.series
+A = scipy.sparse.coo_matrix(([3.0, 1.0, 2.0], ([1, 0, 0], [0, 2, 3])), shape=(100, 100))
+"""
+
+stmt = "ss = pandas.sparse.series.from_coo(A)"
+
+sparse_series_from_coo = Benchmark(stmt, setup, name="sparse_series_from_coo",
+                               start_date=datetime(2015, 1, 3))

smartech7/go-pandas

vb_suite/sparse.py
@@ -37,3 +37,29 @@ stmt = "SparseDataFrame(columns=np.arange(100), index=np.arange(1000))"
 
 sparse_constructor = Benchmark(stmt, setup, name="sparse_frame_constructor",
                                start_date=datetime(2012, 6, 1))
+
+
+setup = common_setup + """
+s = pd.Series([nan] * 10000)
+s[0] = 3.0
+s[100] = -1.0
+s[999] = 12.1
+s.index = pd.MultiIndex.from_product((range(10), range(10), range(10), range(10)))
+ss = s.to_sparse()
+"""
+
+stmt = "ss.to_coo(row_levels=[0, 1], column_levels=[2, 3], sort_labels=True)"
+
+sparse_series_to_coo = Benchmark(stmt, setup, name="sparse_series_to_coo",
+                               start_date=datetime(2015, 1, 3))
+
+setup = common_setup + """
+import scipy.sparse
+import pandas.sparse.series
+A = scipy.sparse.coo_matrix(([3.0, 1.0, 2.0], ([1, 0, 0], [0, 2, 3])), shape=(100, 100))
+"""
+
+stmt = "ss = pandas.sparse.series.from_coo(A)"
+
+sparse_series_from_coo = Benchmark(stmt, setup, name="sparse_series_from_coo",
+                               start_date=datetime(2015, 1, 3))